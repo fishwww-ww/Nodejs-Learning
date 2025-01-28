2fbd8885afcfd1a6b784ef921da10a1450dbdc25
fishwww-ww <2470335462@qq.com>
2025-01-29 01:53:31 +0800

    feat: hellp

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,17 @@
 export default function handler(req, res) {
+  // 只允许 GET 请求
+  if (req.method !== "GET") {
+    return res.status(405).json({
+      error: "Method not allowed",
+      message: "Only GET requests are allowed",
+    });
+  }
+
+  // 从查询参数中获取 name，默认值为 "World"
   const { name = "World" } = req.query;
-  return res.json({
+
+  // 返回 JSON 响应
+  return res.status(200).json({
     message: `Hello ${name}!`,
   });
 }
